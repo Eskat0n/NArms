9c47709dbaa075dc8935fef9b39491cd481a1c2e
eskat0n <muyou.prj@gmail.com>
2012-03-11 23:30:29 +0600

    added NArms.AutoMapper project

---
+++ src/NArms.AutoMapper/MapExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace NArms.AutoMapper
+{
+    using System.Collections.Generic;
+    using global::AutoMapper;
+
+    public static class MapExtensions
+    {
+        public static TDest MapTo<TDest>(this object source)
+        {
+            return (TDest) Mapper.Map(source, source.GetType(), typeof (TDest));
+        }
+
+        public static TDest MapTo<TSource, TDest>(this TSource source, TDest dest)
+        {
+            return Mapper.Map(source, dest);
+        }
+
+        public static IEnumerable<TDest> MapEachTo<TDest>(this IEnumerable<object> source)
+        {
+            return (IEnumerable<TDest>) Mapper.Map(source, source.GetType(), typeof (IEnumerable<TDest>));
+        }
+    }
+}
