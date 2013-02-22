4bb6ab5584d8b861d2c669d690e9684c6159ef11
eskat0n <muyou.prj@gmail.com>
2013-02-22 17:04:50 +0600

    added method for NArms.AutoMapper extensions for explicit destination map

--- src/NArms.AutoMapper/MapExtensions.cs
+++ src/NArms.AutoMapper/MapExtensions.cs
@@ -1,10 +1,23 @@
 ﻿namespace NArms.AutoMapper
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using global::AutoMapper;
 
     public static class MapExtensions
     {
+        public static object MapTo(this object source)
+        {
+            var typeMap = Mapper.GetAllTypeMaps()
+                .SingleOrDefault(x => x.SourceType == source.GetType());
+
+            if (typeMap == null)
+                throw new InvalidOperationException(string.Format("There are two or more mappings for source type {0}", source.GetType()));
+
+            return Mapper.Map(source, source.GetType(), typeMap.DestinationType);
+        }
+
         public static TDest MapTo<TDest>(this object source)
         {
             return (TDest) Mapper.Map(source, source.GetType(), typeof (TDest));
