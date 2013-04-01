bb9d61313c2c1068495fb42232acc23b7fe9858f
eskat0n <muyou.prj@gmail.com>
2013-04-02 01:09:26 +0600

    implemented attribute for ordering installers

---
+++ src/NArms.Windsor/Annotations/InstallerOrderAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace NArms.Windsor.Annotations
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class InstallerOrderAttribute : Attribute
+    {
+        public int Order { get; private set; }
+
+        public InstallerOrderAttribute(int order)
+        {
+            Order = order;
+        }
+    }
+}
